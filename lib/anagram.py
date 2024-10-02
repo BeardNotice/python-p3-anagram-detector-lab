5b11781ec673819b2d9f7e26e8b5a5857fe2d3d4
Ian Flurkey <ianflurkey@gmail.com>
2024-10-02 13:25:28 -0400

    final commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+
+    def match(self, candidates):
+        matches = []
+        for candidate in candidates:
+            if sorted(candidate) == sorted(self.word):
+                matches.append(candidate)
+        return matches
